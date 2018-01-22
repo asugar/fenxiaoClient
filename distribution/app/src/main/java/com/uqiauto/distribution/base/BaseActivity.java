f103c94a8e6952d16b90950af6a676c323f0b63c
xiaoyi <xiaoyi@DESKTOP-ANNIA12>
2018-01-22 13:59:45 +0800

    first add src

---
+++ distribution/app/src/main/java/com/uqiauto/distribution/base/BaseActivity.java
@@ -0,0 +1,118 @@
+package com.uqiauto.distribution.base;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+import com.uqiauto.distribution.utils.ActivityManager;
+import com.uqiauto.distribution.view.MyProgressDialog;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by xiaoyi on 2018/1/22.
+ */
+
+public abstract class BaseActivity extends RxAppCompatActivity {
+
+    protected Activity mContext;
+    protected Bundle mSavedInstanceState;
+    private Unbinder mUnBinder;
+    private MyProgressDialog progressDialog;
+
+    public Activity getContext() {
+        return mContext;
+    }
+
+    //为了给地图使用
+    public Bundle getSavedInstanceState() {
+        return mSavedInstanceState;
+    }
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 设置锁定坚屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//        PushAgent.getInstance(context).onAppStart();
+        setContentView(getLayout());
+        mUnBinder = ButterKnife.bind(this);
+        mContext = this;
+        mSavedInstanceState = savedInstanceState;
+
+        // 每打开一个新页面都加栈里
+        ActivityManager.getInstance().addActivity(this);
+
+        initEventAndData();
+    }
+
+    protected void setToolBar(Toolbar toolbar, String title) {
+        toolbar.setTitle(title);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        ActivityManager.getInstance().finishActivity(this);
+        super.onDestroy();
+
+        mUnBinder.unbind();
+    }
+
+    protected abstract int getLayout();
+
+    protected abstract void initEventAndData();
+
+
+    /**
+     * 启动loading
+     */
+    public void startLoading() {
+        if (progressDialog == null) {
+            progressDialog = MyProgressDialog.createDialog(this);
+        }
+        progressDialog.show();
+    }
+
+    //这个方法dialog不可取消
+    public void startProgressDialog(String content, boolean isCancelDialog) {
+        if (progressDialog == null) {
+            progressDialog = MyProgressDialog.createDialog(this);
+            progressDialog.setCanceledOnTouchOutside(false);//取消dialog点击事件
+            progressDialog.setMessage(content);
+        }
+
+        progressDialog.show();
+    }
+
+
+    /**
+     * 停止loading
+     */
+    public void stopLoading() {
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+            progressDialog = null;
+        }
+    }
+
+
+}
---
+++ distribution/app/src/main/java/com/uqiauto/distribution/base/BaseFragment.java
@@ -0,0 +1,125 @@
+package com.uqiauto.distribution.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.trello.rxlifecycle2.components.support.RxFragment;
+import com.uqiauto.distribution.view.MyProgressDialog;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by xiaoyi on 2018/1/22.
+ */
+
+public abstract class BaseFragment extends RxFragment {
+    private Activity context;
+
+    protected View mView;
+
+    private Unbinder unbinder;
+    private MyProgressDialog progressDialog;
+
+    @Override
+    public Activity getContext() {
+        return context;
+    }
+
+    public View getView() {
+        return mView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.context = activity;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(getLayoutId(), null);
+        return mView;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+        initEventAndData();
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        unbinder.unbind();
+    }
+
+
+    protected abstract int getLayoutId();
+
+    protected abstract void initEventAndData();
+
+
+    /**
+     * 启动loading
+     */
+    public void startLoading(String content) {
+        if (progressDialog == null) {
+            progressDialog = MyProgressDialog.createDialog(context);
+        }
+        progressDialog.show();
+    }
+
+    /**
+     * 停止loading
+     */
+    public void stopLoading() {
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+            progressDialog = null;
+        }
+    }
+
+
+    // 兼容先前fragement，后期整理删除
+    protected Handler mHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            onHandlerThread(msg);
+        }
+    };
+
+    /**
+     * dispatch mHandler's handleMessage method
+     *
+     * @param msg
+     */
+    protected void onHandlerThread(Message msg) {
+        switch (msg.what) {
+            case 1:
+                startLoading("");
+                break;
+            case 2:
+                stopLoading();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+}
